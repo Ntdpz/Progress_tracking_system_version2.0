39d4b9edda2ba5bf6c2d0956d4688264c312aaa9
YARDFAH PHUNGPHOPHAN <6331305041@lamduan.mfu.ac.th>
2023-01-31 19:53:50 +0700

    project init

---
+++ frontend/layouts/Nav.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="content mx-auto">
+    <h1>Testing</h1>
+  </div>
+</template>
+
+<script>
+export default {
+ layout: "Nav",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style>
+</style>
