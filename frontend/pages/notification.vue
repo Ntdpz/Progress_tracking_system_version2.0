8452513f6158e8a7ac8d735c42b64e93d76d9d9d
Annopzi <6331305031@lamduan.mfu.ac.th>
2023-02-03 20:31:59 +0700

    Noti page & Profile page

---
+++ frontend/pages/notification.vue
@@ -0,0 +1,108 @@
+<template>
+  <div>
+    <v-row class="mb-3">
+      <v-text-title
+        class="center ml-4 mr-4 mt-3 mb-1"
+        style="font-weight: bold; font-size: 20px"
+      >
+        Notifications
+      </v-text-title>
+      <v-divider
+        class="mt-3 mb-1"
+        inset
+        vertical
+        style="background-color: black"
+      ></v-divider>
+      <template>
+        <v-banner
+          class="mt-3 ml-4"
+          style="
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 30px;
+            border-radius: 30px;
+            padding: 0 0px;
+          "
+          outlined
+          elevation="2"
+        >
+          <form class="center" @submit.prevent="search">
+            <v-icon color="purple">mdi-magnify</v-icon>
+            <input
+              class="mr-3"
+              type="text"
+              v-model="query"
+              placeholder="Search some notification"
+            />
+          </form>
+        </v-banner>
+      </template>
+    </v-row>
+    <v-divider></v-divider>
+    <div>
+      <v-card v-for="n in 1" :key="n" class="mt-10 mb-10" elevation="8">
+        <v-card-title>Stock Product {{ n }}</v-card-title>
+        <v-row class="ml-0 pl-3">
+          <v-icon color="green">mdi-circle</v-icon>
+          <v-card-subtitle style="font-size: 19px; color: black"
+            >Screen 1</v-card-subtitle
+          >
+        </v-row>
+        <v-row class="ml-12 mt-0 mb-0 mr-0">
+          <v-icon color="black">mdi-account-circle</v-icon>
+          <v-card-subtitle style="font-size: 19px; color: black"
+            >Dev 1 Status: Complete</v-card-subtitle
+          >
+        </v-row>
+      </v-card>
+
+      <v-card v-for="n in 1" :key="n" class="mt-10 mb-10" elevation="8">
+        <v-card-title>Reset Password {{ n }}</v-card-title>
+        <v-row class="ml-0 pl-3">
+          <v-icon color="red">mdi-circle</v-icon>
+          <v-card-subtitle style="font-size: 19px; color: black"
+            >Reset Password</v-card-subtitle
+          >
+        </v-row>
+        <v-row class="ml-12 mt-0 mb-0 mr-0">
+          <v-icon color="black">mdi-account-circle</v-icon>
+          <v-card-subtitle style="font-size: 19px; color: black"
+            >Dev 2 Need reset Password</v-card-subtitle
+          >
+        </v-row>
+      </v-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      query: "",
+    };
+  },
+  methods: {
+    search() {
+      // Perform your search logic here
+      console.log(`Searching for ${this.query}`);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+input[type="text"] {
+  border: black;
+  font-size: 16px;
+  padding-left: 10px;
+  outline: black;
+}
+</style>
---
+++ frontend/pages/profile.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <v-container class="mt-4">
+            <v-row class="mb-6" no-gutters>
+                <v-col cols="3">
+                    <!-- เอาเส้นออกใส่ border: none; -->
+                    <v-card class="pa-2" tile outlined style="height: 465px; display: flex; justify-content: center">
+                        <div>
+                            <v-avatar class="mt-6 mx-auto" style="width: 150px; height: 150px">
+                                <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+                            </v-avatar>
+                            <v-btn icon style="color: white; background-color: purple; float: right;
+                                margin-left: -42px; margin-top: 131px; position: absolute;">
+                                <v-icon>mdi-pencil</v-icon>
+                            </v-btn>
+                        </div>
+                    </v-card>
+                </v-col>
+                <v-col cols="9">
+                    <!-- เอาเส้นออกใส่ border: none; -->
+                    <v-card class="pa-2" tile outlined style="height: 465px">
+                        <div class="mt-6 ml-15">
+                            <h1>Grid col-9 information</h1>
+                        </div>
+                        <v-btn elevation="2" color="purple" bottom right absolute
+                            style="color: white; border-radius: 10px">Update
+                        </v-btn>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+.center {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
