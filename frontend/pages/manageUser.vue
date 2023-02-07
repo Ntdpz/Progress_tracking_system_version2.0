9dd956858529b3b53da32393285c46ed12ec7d4f
Annopzi <6331305031@lamduan.mfu.ac.th>
2023-02-08 01:23:09 +0700

    update

--- frontend/pages/manageUser.vue
+++ frontend/pages/manageUser.vue
@@ -23,138 +23,88 @@
     </v-row>
     <v-divider></v-divider>
     <!-- class="green lighten-5" -->
-    <v-container >
-      <v-row no-gutters >
-    <!-- เอาเส้นออกใส่ border: none; -->
-            <v-card class="pa-2" tile outlined style="border: none; width: 100%; height: 100%; display: flex; justify-content: center">
-              <v-col col="12" sm="12" md="12">
-              <!-- หมวดหมู่ หัวข้อใหญ่ -->
-              <v-toolbar class="mb-2">
-                <v-tabs fixed-tabs v-model="tab" next-icon="mdi-menu-right-outline" prev-icon="mdi-menu-left-outline"
-                  show-arrows>
-                  <v-tabs-slider color="primary"></v-tabs-slider>
-                  <v-tab v-for="item in items" :key="item" style="font-weight: bold; color: black;">
-                    {{ item }}
-                  </v-tab>
-                </v-tabs>
-              </v-toolbar>
-              <!-- ตารางย่อยในหัวข้อ -->
-              <v-tabs-items v-model="tab" >
-                <!-- <v-tab-item v-for="item in items" :key="item">
+    <v-container>
+      <v-row no-gutters>
+        <!-- เอาเส้นออกใส่ border: none; -->
+        <v-card class="pa-2" tile outlined style=" width: 100%; height: 100%; display: flex; justify-content: center">
+          <v-col col="12" sm="12" md="12">
+            <!-- หมวดหมู่ หัวข้อใหญ่ -->
+            <v-toolbar class="mb-2">
+              <v-tabs fixed-tabs v-model="tab" next-icon="mdi-menu-right-outline" prev-icon="mdi-menu-left-outline"
+                show-arrows>
+                <v-tabs-slider color="primary"></v-tabs-slider>
+                <v-tab v-for="item in items" :key="item" style="font-weight: bold; color: black;">
+                  {{ item }}
+                </v-tab>
+              </v-tabs>
+            </v-toolbar>
+            <!-- ตารางย่อยในหัวข้อ -->
+            <v-tabs-items v-model="tab">
+              <!-- <v-tab-item v-for="item in items" :key="item">
                   <v-card flat>
                     <v-card-text v-text="text"></v-card-text>
                   </v-card>
                 </v-tab-item> -->
-                <!-- Tab1 -->
-                <v-tab-item>
-                  <v-card flat>
-                    <!--  -->
-                    <v-data-table :headers="headers" :items="desserts" sort-by="calories" class="elevation-1">
-                      <template v-slot:top>
-                        
-                          
-                          <v-dialog v-model="dialog" max-width="500px">
-                            <v-card>
-                              <v-card-text>
-                                <v-container>
-                                  <v-row>
-                                    <v-col cols="12" sm="6" md="4">
-                                      <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field>
-                                    </v-col>
-                                    <v-col cols="12" sm="6" md="4">
-                                      <v-text-field v-model="editedItem.calories" label="Calories"></v-text-field>
-                                    </v-col>
-                                    <v-col cols="12" sm="6" md="4">
-                                      <v-text-field v-model="editedItem.fat" label="Fat (g)"></v-text-field>
-                                    </v-col>
-                                    <v-col cols="12" sm="6" md="4">
-                                      <v-text-field v-model="editedItem.carbs" label="Carbs (g)"></v-text-field>
-                                    </v-col>
-                                    <v-col cols="12" sm="6" md="4">
-                                      <v-text-field v-model="editedItem.protein" label="Protein (g)"></v-text-field>
-                                    </v-col>
-                                  </v-row>
-                                </v-container>
-                              </v-card-text>
-                    
-                              <v-card-actions>
-                                <v-spacer></v-spacer>
-                                <v-btn color="blue darken-1" text @click="close">
-                                  Cancel
-                                </v-btn>
-                                <v-btn color="blue darken-1" text @click="save">
-                                  Save
-                                </v-btn>
-                              </v-card-actions>
-                            </v-card>
-                          </v-dialog>
-                          <v-dialog v-model="dialogDelete" max-width="500px">
-                            <v-card>
-                              <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
-                              <v-card-actions>
-                                <v-spacer></v-spacer>
-                                <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-                                <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
-                                <v-spacer></v-spacer>
-                              </v-card-actions>
-                            </v-card>
-                          </v-dialog>
-                       
-                      </template>
-                      <template v-slot:[`item.actions`]="{ item }">
-                        <v-icon small class="mr-2" @click="editItem(item)">
-                          mdi-pencil
-                        </v-icon>
-                        <v-icon small @click="deleteItem(item)">
-                          mdi-delete
-                        </v-icon>
-                      </template>
-                      <template v-slot:no-data>
-                        <v-btn color="primary" @click="initialize">
-                          Reset
-                        </v-btn>
-                      </template>
-                    </v-data-table>
-                    <!--  -->
-                  </v-card>
-                </v-tab-item>
-                <!--  -->
-                <v-tab-item>
-                  <v-card flat>
-                    <v-card-text>Tab2</v-card-text>
-                  </v-card>
-                </v-tab-item>
-                <v-tab-item>
-                  <v-card flat>
-                    <v-card-text>Tab3</v-card-text>
-                  </v-card>
-                </v-tab-item>
-                <v-tab-item>
-                  <v-card flat>
-                    <v-card-text>Tab4</v-card-text>
-                  </v-card>
-                </v-tab-item>
-                <v-tab-item>
-                  <v-card flat>
-                    <v-card-text>Tab5</v-card-text>
-                  </v-card>
-                </v-tab-item>
-                <v-tab-item>
-                  <v-card flat>
-                    <v-card-text>Tab6</v-card-text>
-                  </v-card>
-                </v-tab-item>
+              <!-- Tab1 -->
+              <v-tab-item>
+                <v-card flat>
+                  <!--  -->
+                  <v-data-table :headers="headers" :items="desserts" sort-by="calories" class="elevation-1">
+                    <template v-slot:[`item.actions`]="{ item }">
+                      <v-icon small class="mr-2" @click="editItem(item)">
+                        mdi-pencil
+                      </v-icon>
+                      <v-icon small @click="deleteItem(item)">
+                        mdi-delete
+                      </v-icon>
+                    </template>
+                    <template v-slot:no-data>
+                      <v-btn color="primary" @click="initialize">
+                        Reset
+                      </v-btn>
+                    </template>
+                  </v-data-table>
+                  <!--  -->
+                </v-card>
+              </v-tab-item>
+              <!--  -->
+              <v-tab-item>
+                <v-card flat>
+                  <v-card-text>Tab2</v-card-text>
+                </v-card>
+              </v-tab-item>
+              <v-tab-item>
+                <v-card flat>
+                  <v-card-text>Tab3</v-card-text>
+                </v-card>
+              </v-tab-item>
+              <v-tab-item>
+                <v-card flat>
+                  <v-card-text>Tab4</v-card-text>
+                </v-card>
+              </v-tab-item>
+              <v-tab-item>
+                <v-card flat>
+                  <v-card-text>Tab5</v-card-text>
+                </v-card>
+              </v-tab-item>
+              <v-tab-item>
+                <v-card flat>
+                  <v-card-text>Tab6</v-card-text>
+                </v-card>
+              </v-tab-item>
 
-              </v-tabs-items>
-              
-              </v-col>
-              
-            </v-card> 
-        
-        
+            </v-tabs-items>
+
+          </v-col>
+
+        </v-card>
+
+
       </v-row>
       <!--  -->
-      <v-btn class="ml-2 mr-10 mb-0 " elevation="2" color="primary" bottom right absolute style="color: white; border-radius: 10px">
+      <v-btn class="ml-2 mr-10 mb-0 " elevation="2" color="primary" bottom right absolute
+        style="color: white; border-radius: 10px">
         <v-icon left>
           mdi-plus-circle-outline
         </v-icon>
@@ -179,15 +129,17 @@ export default {
       dialogDelete: false,
       headers: [
         {
-          text: 'Dessert (100g serving)',
+          text: 'Photo',
           align: 'start',
           sortable: false,
-          value: 'name',
+          value: 'photo',
         },
-        { text: 'Calories', value: 'calories' },
-        { text: 'Fat (g)', value: 'fat' },
-        { text: 'Carbs (g)', value: 'carbs' },
-        { text: 'Protein (g)', value: 'protein' },
+        { text: 'Name', value: 'name' },
+        { text: 'Code', value: 'code' },
+        { text: 'Position', value: 'position' },
+        { text: 'Department', value: 'department' },
+        { text: 'Status', value: 'status' },
+        { text: 'Role', value: 'role' },
         { text: 'Actions', value: 'actions', sortable: false },
       ],
       desserts: [],
@@ -212,79 +164,65 @@ export default {
     initialize() {
       this.desserts = [
         {
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
+          photo: '',
+          name: 'Mr. Implementer1',
+          code: 'AI00000001',
+          position: 'Implementer',
+          department: 'DD',
+          status: 'Active',
+          role: 'User',
         },
         {
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
+          photo: '',
+          name: 'Mr. Implementer1',
+          code: 'AI00000001',
+          position: 'Implementer',
+          department: 'DD',
+          status: 'Active',
+          role: 'User',
         },
         {
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
+          photo: '',
+          name: 'Mr. Implementer1',
+          code: 'AI00000001',
+          position: 'Implementer',
+          department: 'DD',
+          status: 'Active',
+          role: 'User',
         },
         {
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
+          photo: '',
+          name: 'Mr. Implementer1',
+          code: 'AI00000001',
+          position: 'Implementer',
+          department: 'DD',
+          status: 'Active',
+          role: 'User',
         },
         {
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-        },
-        {
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-        },
-        {
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-        },
-        {
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-        },
-        {
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-        },
-        {
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
+          photo: '',
+          name: 'Mr. Implementer1',
+          code: 'AI00000001',
+          position: 'Implementer',
+          department: 'DD',
+          status: 'Active',
+          role: 'User',
         },
       ]
-    }
-},
-  
+    },
+    editItem(item) {
+      this.editedIndex = this.desserts.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+
+    deleteItem(item) {
+      this.editedIndex = this.desserts.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialogDelete = true
+    },
+  },
+
 
 }
 </script>
