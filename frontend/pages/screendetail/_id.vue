3253034a51fe59c8be6d6143e77148d86fe6e1ab
Annopzi <6331305031@lamduan.mfu.ac.th>
2023-02-10 16:08:15 +0700

    Update Screendetail

--- frontend/pages/screendetail/_id.vue
+++ frontend/pages/screendetail/_id.vue
@@ -15,7 +15,7 @@
                                       border-radius: 30px;
                                       padding: 0 0px;
                                     " outlined elevation="2">
-                    <form class="center" @submit.prevent="search">
+                    <form class="center">
                         <v-icon color="purple">mdi-magnify</v-icon>
                         <input class="mr-3" type="text" v-model="query" placeholder="Search some screen" />
                     </form>
@@ -41,13 +41,86 @@
             <!-- รูป กับรายลละเอียด -->
             <v-row no-gutters>
                 <v-col col="6" sm="6" md="6">
-                    <v-card outlined tile>
-                        <h1>รูป</h1>
+                    <v-card style="" class="ma-1 mt-0" tile>
+                        <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="430px"></v-img>
                     </v-card>
                 </v-col>
                 <v-col col="6" sm="6" md="6">
-                    <v-card outlined tile>
-                        <h1>รายละเอียด</h1>
+                    <v-card outlined tile height="430px">
+                        <v-container fluid>
+                            <v-row>
+                                <v-col cols="4" style="place-self: center;">
+                                    <h5 class="">Screen Code</h5>
+                                </v-col>
+                                <v-col cols="8" >
+                                    <v-text-field style="text-align-last: center;" v-model="screencode" hide-details="auto" dense
+                                        outlined></v-text-field>
+                                </v-col>
+                            </v-row>
+                            <v-row>
+                                <v-col cols="4" style="place-self: center;">
+                                    <h5 class="">Screen Name</h5>
+                                </v-col>
+                                <v-col cols="8">
+                                    <v-text-field style="text-align-last: center;" v-model="screenname" hide-details="auto" dense
+                                        outlined></v-text-field>
+                                </v-col>
+                            </v-row>
+                            <v-row>
+                                <v-col cols="4" style="place-self: center;">
+                                    <h5 class="">Developer</h5>
+                                </v-col>
+                                <v-col cols="8">
+                                    <v-text-field style="text-align-last: center;" v-model="developer" hide-details="auto" dense outlined></v-text-field>
+                                </v-col>
+                            </v-row>
+                            <v-row>
+                                <v-col cols="4" style="place-self: center;">
+                                    <h5 class="">Implementer</h5>
+                                </v-col>
+                                <v-col cols="8">
+                                    <v-text-field style="text-align-last: center;" v-model="implementer" hide-details="auto" dense
+                                        outlined></v-text-field>
+                                </v-col>
+                            </v-row>
+                            <v-row>
+                                <v-col cols="2" style="place-self: center;">
+                                    <h5 class="">Status</h5>
+                                </v-col>
+                                <v-col cols="4">
+                                    <template>
+                                        <v-text-field style="text-align-last: center;" v-model="status" hide-details="auto" readonly dense outlined>
+                                            <template v-slot:prepend-inner>
+                                                <v-icon
+                                                    :color="status === 'Complete' ? 'green' : 'red'">mdi-circle</v-icon>
+                                            </template>
+                                        </v-text-field>
+                                    </template>
+
+                                </v-col>
+                                <v-col cols="2" style="place-self: center;">
+                                    <h5 class="">Level</h5>
+                                </v-col>
+                                <v-col cols="4">
+                                    <v-text-field style="text-align-last: center;" v-model="level" hide-details="auto" dense outlined></v-text-field>
+                                </v-col>
+                            </v-row>
+                            <v-row>
+                                <v-col cols="3" style="place-self: center;">
+                                    <h5 class="">Manday</h5>
+                                </v-col>
+                                <v-col cols="4">
+                                    <v-text-field style="text-align-last: center;" v-model="manday" hide-details="auto" dense outlined></v-text-field>
+                                </v-col>
+                            </v-row>
+                            <!--  -->
+                            <v-btn to="#" class="mr-2" elevation="2" color="primary" bottom right absolute
+                                style="color: white; border-radius: 10px">Update
+                            </v-btn>
+                            <!--  -->
+                        </v-container>
+
+
                     </v-card>
                 </v-col>
             </v-row>
@@ -60,7 +133,15 @@ export default {
     layout: 'admin',
     data() {
         return {
-            query:'',
+            query: '',
+            showIcon: true,
+            screencode: 'A001',
+            screenname: 'List Page',
+            developer: 'Developer 1',
+            implementer: 'Implements 1',
+            status: 'Not Complete',
+            level: 100,
+            manday: '365 days',
             id: this.$route.params.id,
         };
     },
@@ -68,7 +149,6 @@ export default {
 </script>
 
 <style scoped>
-
 input[type="text"] {
     border: black;
     font-size: 16px;
