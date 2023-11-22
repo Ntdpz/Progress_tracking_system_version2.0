290ad1a5cd964276f6c5474d85a367797918fddd
Gluay-master <6331305031@lamduan.mfu.ac.th>
2023-11-22 23:15:52 +0700

    Update Dialog

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: true }));
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
