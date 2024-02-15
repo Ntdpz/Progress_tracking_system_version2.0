4b259827f5e0b0e9d0267f20e676fb62ab72fd51
Naphat <6431503016@lamduan.mfu.ac.th>
2024-02-15 16:38:30 +0700

    set database defales

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,21 @@
 const mysql = require("mysql");
 
+
 const connection = mysql.createConnection({
   connectionLimit: 10, // จำนวน connection สูงสุดที่ pool สามารถจัดการได้
-  host: '0.0.0.0',     // ที่อยู่ IP หรือชื่อโฮสต์ของ MySQL Server
-  user: 'root',         // ชื่อผู้ใช้ MySQL
-  password: '',         // รหัสผ่าน MySQL
-  database: 'progresstracking_db', // ชื่อฐานข้อมูลที่ใช้
+  host: 'mysql-seniorprojectprogresstracking.alwaysdata.net',     // ที่อยู่ IP หรือชื่อโฮสต์ของ MySQL Server
+  user: '347136',         // ชื่อผู้ใช้ MySQL
+  password: 'Progresstracking1001',         // รหัสผ่าน MySQL
+  database: 'seniorprojectprogresstracking_term1', // ชื่อฐานข้อมูลที่ใช้
 });
-
-// const db = mysql.createPool({
+// const connection = mysql.createConnection({
 //   connectionLimit: 10, // จำนวน connection สูงสุดที่ pool สามารถจัดการได้
 //   host: '0.0.0.0',     // ที่อยู่ IP หรือชื่อโฮสต์ของ MySQL Server
 //   user: 'root',         // ชื่อผู้ใช้ MySQL
 //   password: '',         // รหัสผ่าน MySQL
-//   database: 'graduation_project', // ชื่อฐานข้อมูลที่ใช้
+//   database: 'progresstracking_db', // ชื่อฐานข้อมูลที่ใช้
 // });
+
 
 // const connection = mysql.createConnection({
 //   host: "se.mfu.ac.th",
