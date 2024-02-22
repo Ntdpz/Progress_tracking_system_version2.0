d0c9f28bedac626ecba3504a97ca578e4277d857
Naphat <6431503016@lamduan.mfu.ac.th>
2024-02-22 21:30:20 +0700

    database

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,20 @@
 const mysql = require("mysql");
 
 
-const connection = mysql.createConnection({
-  connectionLimit: 10, // จำนวน connection สูงสุดที่ pool สามารถจัดการได้
-  host: 'mysql-seniorprojectprogresstracking.alwaysdata.net',     // ที่อยู่ IP หรือชื่อโฮสต์ของ MySQL Server
-  user: '347136',         // ชื่อผู้ใช้ MySQL
-  password: 'Progresstracking1001',         // รหัสผ่าน MySQL
-  database: 'seniorprojectprogresstracking_term1', // ชื่อฐานข้อมูลที่ใช้
-});
 // const connection = mysql.createConnection({
 //   connectionLimit: 10, // จำนวน connection สูงสุดที่ pool สามารถจัดการได้
-//   host: '0.0.0.0',     // ที่อยู่ IP หรือชื่อโฮสต์ของ MySQL Server
-//   user: 'root',         // ชื่อผู้ใช้ MySQL
-//   password: '',         // รหัสผ่าน MySQL
-//   database: 'progresstracking_db', // ชื่อฐานข้อมูลที่ใช้
+//   host: 'mysql-seniorprojectprogresstracking.alwaysdata.net',     // ที่อยู่ IP หรือชื่อโฮสต์ของ MySQL Server
+//   user: '347136',         // ชื่อผู้ใช้ MySQL
+//   password: 'Progresstracking1001',         // รหัสผ่าน MySQL
+//   database: 'seniorprojectprogresstracking_term1', // ชื่อฐานข้อมูลที่ใช้
 // });
+const connection = mysql.createConnection({
+  connectionLimit: 10, // จำนวน connection สูงสุดที่ pool สามารถจัดการได้
+  host: '0.0.0.0',     // ที่อยู่ IP หรือชื่อโฮสต์ของ MySQL Server
+  user: 'root',         // ชื่อผู้ใช้ MySQL
+  password: '',         // รหัสผ่าน MySQL
+  database: 'notemanagement_db', // ชื่อฐานข้อมูลที่ใช้
+});
 
 
 // const connection = mysql.createConnection({
