ff53abaf4e91ecbfe12c2870c0a94554256b9b8c
WITSAKON LOTHANARUNGROT <6431503055@lamduan.mfu.ac.th>
2024-03-02 21:18:53 +0700

    update systemoverviw + add edit system

---
+++ frontend/components/EditSystemForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <label for="systemNameTH">System Name (TH):</label>
+    <input type="text" id="systemNameTH" v-model="editedSystem.system_nameTH">
+    <label for="systemNameEN">System Name (EN):</label>
+    <input type="text" id="systemNameEN" v-model="editedSystem.system_nameEN">
+    <label for="systemMember">System Member:</label>
+    <input type="text" id="systemMember" v-model="editedSystem.system_member">
+    <button type="submit">Save</button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    system: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      editedSystem: { ...this.system } // สร้างคัดลอกข้อมูลระบบเพื่อแก้ไข
+    };
+  },
+  methods: {
+    submitForm() {
+      // ส่งข้อมูลที่แก้ไขไปยัง method ที่เพื่อการบันทึก
+      this.$emit('save', this.editedSystem);
+    }
+  }
+};
+</script>
