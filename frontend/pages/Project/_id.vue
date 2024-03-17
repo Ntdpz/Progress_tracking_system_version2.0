6a0777a04f830898f23e30a2d9b605e04be84977
Naphat <6431503016@lamduan.mfu.ac.th>
2024-03-17 23:33:22 +0700

    Systems

--- frontend/pages/Project/_id.vue
+++ frontend/pages/Project/_id.vue
@@ -1,107 +1,182 @@
 <template>
   <!-- Systems Data Table container -->
   <div class="systems-data-table">
+    <v-card class="mx-auto align-start" max-width="800" hover>
+      <v-card-item @click="showDetails = !showDetails">
+        <v-card-title>
+          {{ project.project_name_ENG }}
+        </v-card-title>
+        <v-card-subtitle>
+          Project Progress:
+          <v-progress-linear
+            v-if="project.project_progress !== null"
+            :model-value="project.project_progress"
+          ></v-progress-linear>
+          <v-progress-linear v-else model-value="0"></v-progress-linear>
+        </v-card-subtitle>
+      </v-card-item>
+
+      <v-expand-transition>
+        <div v-show="showDetails">
+          <v-divider></v-divider>
+          <v-card-text>
+            <p>Project Manday: {{ project.project_manday }}</p>
+            <p>System Count: {{ project.system_count }}</p>
+            <p>Project Plan Start: {{ project.project_plan_start }}</p>
+            <p>Project Plan End: {{ project.project_plan_end }}</p>
+          </v-card-text>
+        </div>
+      </v-expand-transition>
+    </v-card>
     <!-- Search bar -->
     <v-row no-gutters>
       <v-col cols="12">
-        <input type="text" v-model="searchQuery" placeholder="Search..." style="
+        <input
+          type="text"
+          v-model="searchQuery"
+          placeholder="Search..."
+          style="
             margin-bottom: 10px;
             width: 100%;
             padding: 10px;
             border: 1px solid #ccc;
             border-radius: 5px;
             font-size: 16px;
-          " />
+          "
+        />
       </v-col>
     </v-row>
     <!-- Data Table -->
-    <v-data-table :headers="headers" :items="filteredSystems" :items-per-page="5" class="elevation-1">
+    <v-data-table
+      :headers="headers"
+      :items="filteredSystems"
+      :items-per-page="5"
+      class="elevation-1"
+    >
       <template v-slot:top>
         <v-toolbar flat>
-          <v-toolbar-title>Systems Management - Project :
-            {{ projectNameENG }}</v-toolbar-title>
+          <v-toolbar-title
+            >Systems Management - Project :
+            {{ projectNameENG }}</v-toolbar-title
+          >
           <v-divider class="mx-4" inset vertical></v-divider>
           <v-spacer></v-spacer>
-          <v-btn color="primary" dark @click="goToCreateSystem">New System</v-btn>
-          <v-btn color="primary" dark @click="goToHistorySystems" style="margin-left: 10px;">Show HistorySystem</v-btn>
+          <v-btn color="primary" dark @click="goToCreateSystem"
+            >New System</v-btn
+          >
+          <v-btn
+            color="primary"
+            dark
+            @click="goToHistorySystems"
+            style="margin-left: 10px"
+            >Show HistorySystem</v-btn
+          >
         </v-toolbar>
-
-        <!-- Create System Dialog -->
-        <v-dialog v-model="createSystemDialog" max-width="600" ref="createSystemDialog">
-          <v-card>
-            <v-card-title>Create New System</v-card-title>
-            <v-card-text>
-              <!-- Form to create new system -->
-              <v-form @submit.prevent="createSystem">
-                <v-text-field v-model="newSystem.system_id" label="System ID"></v-text-field>
-                <v-text-field v-model="newSystem.system_nameTH" label="System Name (TH)"></v-text-field>
-                <v-text-field v-model="newSystem.system_nameEN" label="System Name (EN)"></v-text-field>
-                <v-text-field v-model="newSystem.system_shortname" label="Short Name"></v-text-field>
-
-                <v-btn type="submit">Create</v-btn>
-                <v-btn @click="createSystemDialog = false">Cancel</v-btn>
-              </v-form>
-            </v-card-text>
-          </v-card>
-        </v-dialog>
-
-        <!-- Edit System Dialog -->
-        <v-dialog v-model="editSystemDialog" max-width="600" ref="editSystemDialog">
-          <v-card>
-            <v-card-title>Edit System</v-card-title>
-            <v-card-text>
-              <!-- Form to edit system -->
-              <v-form @submit.prevent="updateSystem">
-                <v-text-field v-model="editedSystem.system_id" label="System ID" readonly></v-text-field>
-                <v-text-field v-model="editedSystem.system_nameTH" label="System Name (TH)"></v-text-field>
-                <v-text-field v-model="editedSystem.system_nameEN" label="System Name (EN)"></v-text-field>
-                <v-text-field v-model="editedSystem.system_shortname" label="Short Name"></v-text-field>
-                <!-- Add more fields as needed -->
-                <!-- You can also add selection fields for system analyst and member -->
-                <!-- Add buttons to submit and cancel -->
-                <v-btn type="submit">Update</v-btn>
-                <v-btn @click="editSystemDialog = false">Cancel</v-btn>
-              </v-form>
-            </v-card-text>
-          </v-card>
-        </v-dialog>
-
-        <!-- Show deleted systems history -->
-        <v-dialog v-model="showHistoryDialog" max-width="800">
-          <v-data-table :headers="headers" :items="deletedSystems">
-            <!-- Define headers for the table -->
-            <template v-slot:top>
-              <v-toolbar flat>
-                <v-toolbar-title>Deleted Systems History</v-toolbar-title>
-                <v-divider class="mx-4" inset vertical></v-divider>
-                <v-spacer></v-spacer>
-              </v-toolbar>
-            </template>
-
-            <!-- Define actions for each row -->
-            <template v-slot:item.actions="{ item }">
-              <v-btn color="primary" @click="restoreSystem(item)">
-                Restore
-              </v-btn>
-              <v-btn color="error" @click="confirmDeleteHistorySystem(item)">
-                Delete
-              </v-btn>
-            </template>
-
-            <!-- Define template when no data is available -->
-            <!-- <template v-slot:no-data>
-              <v-btn color="primary" @click="initialize">Reset</v-btn>
-            </template> -->
-          </v-data-table>
-        </v-dialog>
       </template>
 
       <template v-slot:item.actions="{ item }">
-        <v-icon class="me-2" size="20" px @click="openEditDialog(item)">mdi-pencil-circle</v-icon>
-        <v-icon size="20" px @click="confirmDeleteSystem(item)">mdi-delete-empty</v-icon>
-        <v-btn @click="goToSystemsDetail(item.id)" style="margin-left: 10px;">Systems Detail</v-btn>
+        <v-icon class="me-2" size="20" px @click="openEditDialog(item)"
+          >mdi-pencil-circle</v-icon
+        >
+        <v-icon size="20" px @click="confirmDeleteSystem(item)"
+          >mdi-delete-empty</v-icon
+        >
+        <v-btn @click="goToSystemsDetail(item.id)" style="margin-left: 10px"
+          >Systems Detail</v-btn
+        >
       </template>
     </v-data-table>
+
+    <!-- Create System Dialog -->
+    <v-dialog
+      v-model="createSystemDialog"
+      max-width="600"
+      ref="createSystemDialog"
+    >
+      <v-card>
+        <v-card-title>Create New System</v-card-title>
+        <v-card-text>
+          <!-- Form to create new system -->
+          <v-form @submit.prevent="createSystem">
+            <v-text-field
+              v-model="newSystem.system_id"
+              label="System ID"
+            ></v-text-field>
+            <v-text-field
+              v-model="newSystem.system_nameTH"
+              label="System Name (TH)"
+            ></v-text-field>
+            <v-text-field
+              v-model="newSystem.system_nameEN"
+              label="System Name (EN)"
+            ></v-text-field>
+            <v-text-field
+              v-model="newSystem.system_shortname"
+              label="Short Name"
+            ></v-text-field>
+
+            <v-btn type="submit">Create</v-btn>
+            <v-btn @click="createSystemDialog = false">Cancel</v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+
+    <!-- Edit System Dialog -->
+    <v-dialog v-model="editSystemDialog" max-width="600" ref="editSystemDialog">
+      <v-card>
+        <v-card-title>Edit System</v-card-title>
+        <v-card-text>
+          <!-- Form to edit system -->
+          <v-form @submit.prevent="updateSystem">
+            <v-text-field
+              v-model="editedSystem.system_id"
+              label="System ID"
+              readonly
+            ></v-text-field>
+            <v-text-field
+              v-model="editedSystem.system_nameTH"
+              label="System Name (TH)"
+            ></v-text-field>
+            <v-text-field
+              v-model="editedSystem.system_nameEN"
+              label="System Name (EN)"
+            ></v-text-field>
+            <v-text-field
+              v-model="editedSystem.system_shortname"
+              label="Short Name"
+            ></v-text-field>
+            <!-- Add more fields as needed -->
+            <!-- You can also add selection fields for system analyst and member -->
+            <!-- Add buttons to submit and cancel -->
+            <v-btn type="submit">Update</v-btn>
+            <v-btn @click="editSystemDialog = false">Cancel</v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+
+    <!-- Show deleted systems history -->
+    <v-dialog v-model="showHistoryDialog" max-width="800">
+      <v-data-table :headers="headers" :items="deletedSystems">
+        <!-- Define headers for the table -->
+        <template v-slot:top>
+          <v-toolbar flat>
+            <v-toolbar-title>Deleted Systems History</v-toolbar-title>
+            <v-divider class="mx-4" inset vertical></v-divider>
+            <v-spacer></v-spacer>
+          </v-toolbar>
+        </template>
+
+        <!-- Define actions for each row -->
+        <template v-slot:item.actions="{ item }">
+          <v-btn color="primary" @click="restoreSystem(item)"> Restore </v-btn>
+          <v-btn color="error" @click="confirmDeleteHistorySystem(item)">
+            Delete
+          </v-btn>
+        </template>
+      </v-data-table>
+    </v-dialog>
   </div>
 </template>
 
@@ -113,6 +188,8 @@ export default {
   layout: "admin",
   data() {
     return {
+      showDetails: false,
+      project: {},
       projectNameENG: "",
       showHistoryDialog: false,
       deletedSystems: [],
@@ -154,20 +231,17 @@ export default {
     async fetchProjectNameENG() {
       try {
         const projectId = this.$route.params.id;
-        const response = await fetch(
+        const response = await this.$axios.$get(
           `http://localhost:7777/projects/getOne/${projectId}`
         );
-        if (!response.ok) {
-          throw new Error("Failed to fetch project");
-        }
-        const projectData = await response.json();
-        console.log(projectData); // ตรวจสอบข้อมูลที่ได้รับมา
-        this.projectNameENG = projectData.project_name_ENG; // ใส่ชื่อ field ที่ต้องการแสดง
+        this.project = response;
+        this.projectNameENG = response.project_name_ENG;
       } catch (error) {
         console.error("Error fetching project:", error);
         // Handle error fetching project
       }
     },
+
     async restoreSystem(item) {
       try {
         const confirmResult = await Swal.fire({
@@ -211,7 +285,7 @@ export default {
             "success"
           );
 
-          // เพิ่มบรรทัดนี้เพื่ออัพเดทตารางอัตโนมัติ
+          // Add this line to update the table automatically
           this.fetchDeletedSystems();
         }
       } catch (error) {
@@ -227,18 +301,12 @@ export default {
     async mounted() {
       try {
         const projectId = this.$route.params.id;
-        const response = await fetch(
+        const response = await this.$axios.$get(
           `http://localhost:7777/projects/getOne/${projectId}`
         );
-        if (!response.ok) {
-          throw new Error("Failed to fetch project");
-        }
-        const projectData = await response.json();
-        console.log(projectData); // ตรวจสอบข้อมูลที่ได้รับมา
-        this.projectNameENG = projectData.project_name_ENG; // ใส่ชื่อ field ที่ต้องการแสดง
+        this.project = response;
       } catch (error) {
         console.error("Error fetching project:", error);
-        // Handle error fetching project
       }
     },
 
@@ -294,7 +362,7 @@ export default {
           throw new Error("Failed to fetch deleted systems");
         }
         const deletedSystems = await response.json();
-        console.log(deletedSystems); // ตรวจสอบ deleted systems ที่ได้รับมา
+        console.log(deletedSystems); // Check the received deleted systems
         this.deletedSystems = deletedSystems;
       } catch (error) {
         console.error("Error fetching deleted systems:", error);
@@ -304,6 +372,21 @@ export default {
     async createSystem() {
       const projectId = this.$route.params.id;
       try {
+        // Remove keys with null values
+        Object.keys(this.newSystem).forEach((key) => {
+          if (this.newSystem[key] === null) {
+            delete this.newSystem[key];
+          }
+        });
+
+        // Replace empty string values with null for date fields
+        if (this.newSystem.project_plan_start === "") {
+          this.newSystem.project_plan_start = null;
+        }
+        if (this.newSystem.system_plan_end === "") {
+          this.newSystem.system_plan_end = null;
+        }
+
         const response = await fetch(
           `http://localhost:7777/systems/createSystem`,
           {
@@ -326,16 +409,18 @@ export default {
           system_nameTH: "",
           system_nameEN: "",
           system_shortname: "",
+          project_plan_start: "", // Clear project_plan_start
+          system_plan_end: "", // Clear system_plan_end
         };
         const confirmResult = await Swal.fire({
           icon: "success",
           title: "Success",
           text: "New system created successfully",
-          showConfirmButton: true, // แสดงปุ่ม "OK"
-          allowOutsideClick: false, // ปิดการคลิกภายนอกเพื่อป้องกันการปิดโดยไม่ได้เช็ค
+          showConfirmButton: true, // Show "OK" button
+          allowOutsideClick: false, // Prevent closing by outside click
         });
         if (confirmResult.isConfirmed) {
-          // อัพเดทข้อมูลโดยอัตโนมัติหลังจากสร้างข้อมูลใหม่สำเร็จ
+          // Update data automatically after successful creation
           this.fetchSystems();
         }
       } catch (error) {
@@ -420,16 +505,16 @@ export default {
           cancelButtonColor: "#d33",
           confirmButtonText: "Yes, delete it!",
           showClass: {
-            popup: "animate__animated animate__fadeInDown", // กำหนด animation เมื่อแสดง SweetAlert
+            popup: "animate__animated animate__fadeInDown", // Set animation when showing SweetAlert
           },
           hideClass: {
-            popup: "animate__animated animate__fadeOutUp", // กำหนด animation เมื่อซ่อน SweetAlert
+            popup: "animate__animated animate__fadeOutUp", // Set animation when hiding SweetAlert
           },
         });
         if (confirmResult.isConfirmed) {
           // If user confirms deletion, call deleteSystem method
           await this.deleteSystem(system);
-          // อัพเดทข้อมูลโดยอัตโนมัติหลังจากลบข้อมูล
+          // Update data automatically after deletion
           this.fetchSystems();
         }
       } catch (error) {
